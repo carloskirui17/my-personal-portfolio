dc41e64aa7efd2192bd50342482cd1b776184d43
carloskirui17 <129453813+carloskirui17@users.noreply.github.com>
2023-06-09 19:46:55 +0000

    done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,8 +7,8 @@ export default function About() {
             <section class="bg-white dark:bg-gray-900">
                 <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                     <div class="mx-auto max-w-screen-sm text-center">
-                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Here are `the name of the hospital`. We provide just the care you need.</h2>
-                        <p class="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Check out our awesome <a href="">Staff</a></p>
+                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">Felling down and need motivation</h2>
+                        <p class="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg"> <a target="_blank" href="https://dribbble.com/">Checkout Where I get some of that sweet sweet motivation</a></p>
                     </div>
                 </div>
             </section>
